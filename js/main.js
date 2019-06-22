85b385f626f63ac8257555c308c04722f5cd2e47
Sachit Karki <sachitjungkarki@gmail.com>
2019-06-22 23:10:24 +0545

    rock paper scissors lizard spock updated

--- js/main.js
+++ js/main.js
@@ -8,79 +8,178 @@ const scoreboard = {
   computer: 0
 }
 
+// consider first key as player, and inner key as computer
+const gameRules = {
+  rock: {
+    rock: {
+      result: 'draw',
+      message: 'Both chose Rock'
+    },
+    paper: {
+      result: 'lose',
+      message: 'Paper covers Rock'
+    },
+    scissors: {
+      result: 'win',
+      message: 'Rock crushes Scissors'
+    },
+    lizard: {
+      result: 'win',
+      message: 'Rock crushes Lizard'
+    },
+    spock: {
+      result: 'lose',
+      message: 'Spock vaorizes Rock'
+    }
+  },
+  paper: {
+    rock: {
+      result: 'win',
+      message: 'Paper covers Rock'
+    },
+    paper: {
+      result: 'draw',
+      message: 'Both chose Paper'
+    },
+    scissors: {
+      result: 'lose',
+      message: 'Scissors cuts Paper'
+    },
+    lizard: {
+      result: 'lose',
+      message: 'Lizard eats Paper'
+    },
+    spock: {
+      result: 'win',
+      message: 'Paper disproves Spock'
+    }
+  },
+  scissors: {
+    rock: {
+      result: 'lose',
+      message: 'Rock crushes Scissors'
+    },
+    paper: {
+      result: 'win',
+      message: 'Scissors cuts Paper'
+    },
+    scissors: {
+      result: 'draw',
+      message: 'Both chose Scissors'
+    },
+    lizard: {
+      result: 'win',
+      message: 'Scissors decapitates Lizard'
+    },
+    spock: {
+      result: 'lose',
+      message: 'Spock smashes Scissors'
+    }
+  },
+  lizard: {
+    rock: {
+      result: 'lose',
+      message: 'Rock crushes Lizard'
+    },
+    paper: {
+      result: 'win',
+      message: 'Lizard eats Paper'
+    },
+    scissors: {
+      result: 'lose',
+      message: 'Scissors decapitates Lizard'
+    },
+    lizard: {
+      result: 'draw',
+      message: 'Both chose Lizard'
+    },
+    spock: {
+      result: 'win',
+      message: 'Lizard poisons Spock'
+    }
+  },
+  spock: {
+    rock: {
+      result: 'win',
+      message: 'Spock vaorizes Rock'
+    },
+    paper: {
+      result: 'lose',
+      message: 'Paper disproves Spock'
+    },
+    scissors: {
+      result: 'win',
+      message: 'Spock smashes Scissors'
+    },
+    lizard: {
+      result: 'lose',
+      message: 'Lizard poisons Spock'
+    },
+    spock: {
+      result: 'draw',
+      message: 'Both chose Spock'
+    }
+  }
+}
+
 // Play Game
 const play = (e) => {
   restart.style.display = 'inline-block';
   const playerChoice = e.target.id;
   const computerChoice = getComputerChoice();
-  const winner = getWinner(playerChoice, computerChoice);
-  showWinner(winner, computerChoice);
+  showWinner(playerChoice, computerChoice);
 }
 
 // Get computers choice
 const getComputerChoice = () => {
   const rand = Math.random();
-  if (rand < 0.34) {
+  if (rand < 0.20) {
     return 'rock';
-  } else if (rand <= 0.67) {
+  } else if (rand < 0.40) {
     return 'paper';
-  } else {
+  } else if (rand < 0.60) {
     return 'scissors';
+  } else if (rand < 0.80) {
+    return 'lizard';
+  } else {
+    return 'spock';
   }
 }
 
-// Get game winner
-const getWinner = (p, c) => {
-  if(p === c) {
-    return 'draw';
-  } else if (p === 'rock') {
-    if (c === 'paper') {
-      return 'computer';
-    } else {
-      return 'player';
-    }
-  } else if (p === 'paper') {
-    if (c === 'scissors') {
-      return 'computer';
-    } else {
-      return 'player';
-    }
-  } else if (p === 'scissors') {
-    if (c === 'rock') {
-      return 'computer';
-    } else {
-      return 'player';
-    }
-  }
-}
-
-const showWinner = (winner, computerChoice) => {
-  if(winner == 'player') {
+// Show game winner
+const showWinner = (playerChoice, computerChoice) => {
+  const rule = gameRules[playerChoice][computerChoice];
+  if(rule.result == 'win') {
     // Inc player score
     scoreboard.player++;
     // Show modal result
     result.innerHTML = `
       <h1 class="text-win">You Win</h1>
       <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+      <p>You Chose <strong>${playerChoice.firstLetterUpperCaseInString()}</strong></p>
       <p>Computer Chose <strong>${computerChoice.firstLetterUpperCaseInString()}</strong></p>
+      <p>${rule.message}</p>
     `;
-  } else if(winner == 'computer') {
+  } else if (rule.result == 'lose') {
     // Inc player score
     scoreboard.computer++;
     // Show modal result
     result.innerHTML = `
       <h1 class="text-lose">You Lose</h1>
       <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+      <p>You Chose <strong>${playerChoice.firstLetterUpperCaseInString()}</strong></p>
       <p>Computer Chose <strong>${computerChoice.firstLetterUpperCaseInString()}</strong></p>
+      <p>${rule.message}</p>
     `;
   } else {
     // Show modal result
     result.innerHTML = `
       <h1>It's A Draw</h1>
       <i class="fas fa-hand-${computerChoice} fa-10x"></i>
-      <p>Computer Chose <strong>${computerChoice.firstLetterUpperCaseInString()}</strong></p>
+      <p>${rule.message}</p>
     `;
   }
+
   // Update score
   updateScore();
   modal.style.display = 'block';
